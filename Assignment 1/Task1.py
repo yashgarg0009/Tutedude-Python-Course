fb377ef81a0ef3cc2914eeff05e71d2641ffd534
Yash Garg <yashgarg0906@gmail.com>
2025-10-31 16:38:33 +0530

    Assignments python of course

---
+++ Assignment 1/Task1.py
@@ -0,0 +1,17 @@
+# Task 1: Perform Basic Mathematical Operations
+# Problem Statement: Write a Python program that does the following:
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+#         o	Addition
+#         o	Subtraction
+#         o	Multiplication
+#         o	Division
+# 3.  Displays the results of each operation on the screen.
+
+number1 = int(input("Enter the first number  : "))
+number2 = int(input("Enter the second number  : "))
+
+print(f"Addition : {number1 + number2}")
+print(f"Subtraction : {number1 - number2}")
+print(f"Multiplication : {number1 * number2}")
+print(f"Division : {number1 / number2}")
